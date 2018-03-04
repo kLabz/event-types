efb2e6576b8b05df557663443b2797ea483c7710
k <k@klabz.org>
2018-03-04 21:19:59 +0100

    Added Cordova event types

---
+++ src/eventtypes/CordovaEventType.hx
@@ -0,0 +1,22 @@
+package eventtypes;
+
+@:enum abstract CordovaEventType(String) to String {
+	/**
+		The deviceready event is fired when the cordova application is starting
+		(not resuming from background).
+	*/
+	var DeviceReady = 'deviceready';
+
+	/**
+		The pause event is fired when the cordova applicaiton is moving to the
+		background.
+	*/
+	var Pause = 'pause';
+
+	/**
+		The resume event is fired when the cordova application is returning to
+		the foreground.
+	*/
+	var Resume = 'resume';
+}
+
